ce2f33e7dab393244b09938f21e40ae6a49760f9
iamneveralone <fieldsawarded@naver.com>
2023-08-05 02:10:44 +0900

    settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.UsersConfig',
-    'travels.TravelsConfig',
-    'reviews.ReviewsConfig',
+    'users',
+    'travels',
+    'reviews',
     'rest_framework',
 ]
 
