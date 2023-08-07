ef60f6a808375784690976cb1d70232802fa0615
iamneveralone <fieldsawarded@naver.com>
2023-08-07 12:57:57 +0900

    [#4] 여행 모델 뷰 초기 구현

--- travels/models.py
+++ travels/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Travel(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    deadline = models.DateField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name="participants")
+    max_participation = models.IntegerField(null=True, blank=True)
+    host_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'host')
+    description = models.TextField(blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    
--- travels/views.py
+++ travels/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Travel
+from .serializers import TravelSerializer
+
+class TravelListView(ListAPIView):
+    queryset = Travel.objects.all()
+    serializer_class = TravelSerializer
+    permission_classes = [AllowAny]
