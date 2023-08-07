0253cdbcfa1113f8a076737ac81fb40858558c1f
iamneveralone <fieldsawarded@naver.com>
2023-08-07 13:43:16 +0900

    [#4] 여행 상세 뷰 구현

--- travels/urls.py
+++ travels/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import TravelListView
+from .views import TravelListView, TravelDetailView
 urlpatterns = [
     path('list/', TravelListView.as_view()),
+    path('detail/<str:travel_name>', TravelDetailView.as_view()),
 ]
