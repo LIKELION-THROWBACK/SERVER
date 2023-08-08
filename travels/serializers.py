ab64da330a56d9da6cf774367ef5c57451c4955b
iamneveralone <fieldsawarded@naver.com>
2023-08-08 19:07:02 +0900

    [#4] 호스트 프로필 이미지 포함

--- travels/serializers.py
+++ travels/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from datetime import date
+from urllib.parse import quote
 
 from .models import Travel
 from users.models import User
@@ -10,13 +11,20 @@ class TravelListSerializer(ModelSerializer):
         queryset=User.objects.all(),
         slug_field='name'
     )
+    host_profile_image = serializers.SerializerMethodField()
     current_member = serializers.SerializerMethodField()
     class Meta:
         model = Travel
-        fields = ['name', 'image', 'start_date', 'end_date', 'host', 'current_member', 'max_participation']
+        fields = ['name', 'image', 'start_date', 'end_date', 'host', 'host_profile_image', 'current_member', 'max_participation']
 
-    def get_current_member(request, obj):
+    def get_current_member(self, obj):
         return obj.members.count()
+    
+    def get_host_profile_image(self, obj):
+        try:
+            return obj.host.profile_image.url
+        except ValueError:
+            return None
 
 
 class TravelDetailSerializer(ModelSerializer):
@@ -29,14 +37,22 @@ class TravelDetailSerializer(ModelSerializer):
         queryset=User.objects.all(),
         slug_field='name'
     )
+    host_profile_image = serializers.SerializerMethodField()
     left_day = serializers.SerializerMethodField()
     current_member = serializers.SerializerMethodField()
     class Meta:
         model = Travel
         fields = '__all__'
 
-    def get_left_day(request, obj):
+    def get_left_day(self, obj):
         return (obj.deadline - date.today()).days
     
-    def get_current_member(request, obj):
+    def get_current_member(self, obj):
         return obj.members.count()
+    
+    def get_host_profile_image(self, obj):
+        print(obj.host.profile_image)
+        print(obj.host.profile_image.url)
+        if obj.host.profile_image:
+            return obj.host.profile_image.url
+        return None
