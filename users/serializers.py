3c5a3ffd7803f8eb1f8a73cd54c9c3b4efd9628f
Als-ET <et_oh@naver.com>
2023-08-09 23:04:12 +0900

    [#1] serializer와 view 수정, 작동확인을 위해 urls.py 수정

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,4 @@
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from datetime import date
-from urllib.parse import quote
 
 from .models import User
 
