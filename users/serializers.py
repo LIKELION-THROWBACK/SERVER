669691d21362fb8e46dd3b5c898564b6b7fdc1de
Als-ET <et_oh@naver.com>
2023-08-11 02:17:12 +0900

    [#1] serializer 구체화

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,27 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from datetime import date
 
 from .models import User
+from reviews.models import Review
+from travels.models import Travel
 
 class UserSerializer(ModelSerializer):
-  class Meta :
-    model = User
-    fields = '__all__'
+    my_reviews = serializers.SerializerMethodField()
+    upcoming_travels_count = serializers.SerializerMethodField()
+    finished_travels_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def get_upcoming_travels_count(self, obj):
+        today = date.today()
+        return Travel.objects.filter(members=obj, start_date__gte=today).count()
+
+    def get_finished_travels_count(self, obj):
+        today = date.today()
+        return Travel.objects.filter(members=obj, end_date__lt=today).count()
+
+    def get_my_reviews(self, obj):
+        return Review.objects.filter(user=obj).values('title', 'image', 'description', 'created_at')
--- users/views.py
+++ users/views.py
@@ -1,14 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework.filters import SearchFilter
-from rest_framework.decorators import action
 from datetime import date
 from .models import User
 from travels.models import Travel
 from reviews.models import Review
 from .serializers import UserSerializer 
-from travels.serializers import TravelListSerializer
 from reviews.serializers import ReviewSerializer
 
 class UserViewSet(ModelViewSet):
@@ -16,21 +13,18 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-    @action(detail=True, methods=['GET'])
     def upcoming_travels_count(self, request, *args, **kwargs):
         user = self.get_object()
         today = date.today()
         upcoming_travels_count = Travel.objects.filter(members=user, start_date__gt=today).count()
         return Response({'upcoming_travels_count': upcoming_travels_count})
 
-    @action(detail=True, methods=['GET'])
     def finished_travels_count(self, request, *args, **kwargs):
         user = self.get_object()
         today = date.today()
         finished_travels_count = Travel.objects.filter(members=user, end_date__lt=today).count()
         return Response({'finished_travels_count': finished_travels_count})
 
-    @action(detail=True, methods=['GET'])
     def my_reviews(self, request, *args, **kwargs):
         user = self.get_object()
         reviews = Review.objects.filter(user=user)
