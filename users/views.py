d65f50ba98868d72b2f214d2e961e681955c7fb6
Als-ET <et_oh@naver.com>
2023-08-11 17:09:46 +0900

    [#1] 불필요 코드 제거

--- users/views.py
+++ users/views.py
@@ -1,32 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from datetime import date
+
 from .models import User
-from travels.models import Travel
-from reviews.models import Review
 from .serializers import UserSerializer 
-from reviews.serializers import ReviewSerializer
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-
-    def upcoming_travels_count(self, request, *args, **kwargs):
-        user = self.get_object()
-        today = date.today()
-        upcoming_travels_count = Travel.objects.filter(members=user, start_date__gt=today).count()
-        return Response({'upcoming_travels_count': upcoming_travels_count})
-
-    def finished_travels_count(self, request, *args, **kwargs):
-        user = self.get_object()
-        today = date.today()
-        finished_travels_count = Travel.objects.filter(members=user, end_date__lt=today).count()
-        return Response({'finished_travels_count': finished_travels_count})
-
-    def my_reviews(self, request, *args, **kwargs):
-        user = self.get_object()
-        reviews = Review.objects.filter(user=user)
-        serializer = ReviewSerializer(reviews, many=True)
-        return Response(serializer.data)
