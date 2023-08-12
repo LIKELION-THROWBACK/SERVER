da2dbfff8d7eedb5ec6680b3423570dbff3e01b0
Als-ET <et_oh@naver.com>
2023-08-12 16:24:06 +0900

    cors 에러 해결

--- config/settings.py
+++ config/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'travels',
     'reviews',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhosst:5173',
+]
