cf0ee4776f8f2c3af92a2f58374fe408fe8720d7
Als-ET <et_oh@naver.com>
2023-08-14 23:18:55 +0900

    배포용 settings 와 requirements 1차수정

--- config/settings.py
+++ config/settings.py
@@ -45,11 +45,9 @@ INSTALLED_APPS = [
     'travels',
     'reviews',
     'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,7 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhosst:5173',
-]
